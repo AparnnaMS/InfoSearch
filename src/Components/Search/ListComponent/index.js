cd22a90f27cb371a974c60a5b53c28cc597c07c8
Aparnna M S <aparnnams.knr@gmail.com>
2020-04-01 17:16:35 +0530

    initial updates

---
+++ src/Components/Search/ListComponent/index.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+
+class ListComponent extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            searchkey:'',
+            name:'',
+            id:'',
+            gender:'',
+         }
+    }
+        
+
+    render(){
+        
+        return(
+            <div className="wrapper">
+           {this.props.searchdata.length > 0 && <table>
+               <thead>
+                   <tr>
+                   <td>ID</td>
+                   <td>Name</td>
+                   <td>Gender</td>
+                
+                   </tr>
+               </thead>
+               <tbody>
+                   {this.props.searchdata.map(data=>{
+                       return(
+                        <tr key={data.id}>
+                        <td>{data.id}</td>
+                        <td>{data.name}</td>
+                        <td>{data.gender}</td>
+                        
+                   </tr>
+                       )
+                   })
+                }
+               </tbody>
+           </table>
+           }
+           {
+               this.props.loading && <div>
+                  <div class="loader">Loading</div>
+               </div>
+           }
+           {
+               
+              
+               this.props.nodata && <div>
+                 <b>Sorry No Matches found.</b> 
+               </div>
+           }
+           </div>
+        );
+
+        
+
+    }
+
+}
+export default ListComponent;
